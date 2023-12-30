8658dfd9ce076d2d5c07c28d158069e312274c87
Suhaib Murshid <suhib.mourshid@gmail.com>
2023-12-30 17:55:24 +0300

    Add method calculateMemoryUsage to src/methods.php

--- src/methods.php
+++ src/methods.php
@@ -34,3 +34,17 @@ function findMissingNumber(array $numbers): int
     );
 }
 
+/**
+ * Calculates the memory usage before and after performing on an array, and return the difference.
+ * 
+ * @param array $arr An array to operate on.
+ * @return int The difference in memory usage before and after the operation.
+ */
+function calculateMemoryUsage(array $arr):int
+{
+    $before = memory_get_usage() . "\n";
+
+    rsort($arr);
+
+    return memory_get_usage() - $before . "\n";
+}
