64fcb5e2f271606114f0568bb1bb399dd9d710f0
Suhaib Murshid <suhib.mourshid@gmail.com>
2023-12-30 17:53:07 +0300

    Create test FindMissingNumberTest in tests

---
+++ tests/FindMissingNumberTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Assignment\Tests\FindMissingNumberTest;
+
+use PHPUnit\Framework\TestCase;
+
+
+class FindMissingNumberTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider inputArray()
+     */
+    public function it_returns_true_for_valid_email_format($inputArray, $result)
+    {
+        $this->assertEquals($result,
+            \Assignment\Src\Methods\findMissingNumber($inputArray)
+        );
+    }
+
+
+    public static function inputArray()
+    {
+        return [
+            [[4,6], 5],
+            [[1,2,3,4,5,7,8,9], 6],
+            [[5,6,7,9], 8],
+            [[9,8,7,6,5,3,2], 4],
+        ];
+    }
+}
+
+
